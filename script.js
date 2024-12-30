b9a313b8777618dfc08a7226ef2f0b2be427ef87
Teo4268 <mamacook3300@gmail.com>
2024-12-30 22:17:10 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('back-button').addEventListener('click', function() {
+    window.history.back();
+});
+
+document.getElementById('search-bar').addEventListener('input', function() {
+    // Logic to handle search input
+});
+
+document.getElementById('play').addEventListener('click', function() {
+    // Logic to play music
+});
+
+document.getElementById('pause').addEventListener('click', function() {
+    // Logic to pause music
+});
+
+document.getElementById('shuffle').addEventListener('click', function() {
+    // Logic to shuffle music
+});
+
+document.getElementById('remove-search').addEventListener('click', function() {
+    // Logic to remove recent search
+});
